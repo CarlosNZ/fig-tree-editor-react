ee6ad9ac298f07071c5e152587c8ca2ca6f656be
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-02-13 15:21:30 +1300

    Tidy up imports

--- demo/src/_imports.ts
+++ demo/src/_imports.ts
@@ -7,19 +7,7 @@
  */
 
 // Published
-import { FigTreeEditor } from 'fig-tree-editor-react'
+// export * from 'fig-tree-editor-react'
 
 // Local
-// import { FigTreeEditor } from './expression-builder/src'
-
-export { FigTreeEditor }
-
-/**
- * FIG TREE EVALUATOR
- */
-
-// Published
-export * from 'fig-tree-evaluator'
-
-// Local
-// export * from './expression-builder/src/fig-tree-evaluator/src'
+export * from './expression-builder/src'
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import FigTreeEditor, { type FigTreeEditorProps } from './FigTreeEditor'
 import { Select } from './Select'
 
 export { FigTreeEditor, Select, FigTreeEditorProps }
+export * from 'fig-tree-evaluator'
