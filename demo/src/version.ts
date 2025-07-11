b94eae5c384b4cae2b47214956f4976eee75668e
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-07-12 04:12:59 +1200

    Update demo

--- demo/src/version.ts
+++ demo/src/version.ts
@@ -1,2 +1,2 @@
-export const figTreeEditorReactVersion = '0.7.3'
-export const timestamp = '7/11/2025, 5:33:03 PM'
+export const figTreeEditorReactVersion = '0.7.4'
+export const timestamp = '7/12/2025, 4:12:09 AM'
