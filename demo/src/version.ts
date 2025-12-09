b332c3c94fef81a4df5432d7b1df6eb56f5a58d7
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-12-10 09:22:11 +1300

    Upgrade evaluator package version in demo, publish

--- demo/src/version.ts
+++ demo/src/version.ts
@@ -1,2 +1,2 @@
-export const figTreeEditorReactVersion = '0.7.11'
-export const timestamp = '12/3/2025, 12:11:17 PM'
+export const figTreeEditorReactVersion = '0.8.0'
+export const timestamp = '12/10/2025, 9:19:54 AM'
