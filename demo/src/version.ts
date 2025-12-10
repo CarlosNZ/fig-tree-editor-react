793bbe5626ef1b4b50cd850eeecebf8cb17b63a1
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-12-10 14:23:24 +1300

    Upgrade fig-tree and publish/deploy

--- demo/src/version.ts
+++ demo/src/version.ts
@@ -1,2 +1,2 @@
-export const figTreeEditorReactVersion = '0.8.0'
-export const timestamp = '12/10/2025, 9:19:54 AM'
+export const figTreeEditorReactVersion = '0.8.1'
+export const timestamp = '12/10/2025, 2:21:15 PM'
