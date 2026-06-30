03ff405c3267f3f5ebc3a033fe3cf86f7d1d1d92
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2026-06-30 12:38:56 +1200

    Add changelog, export containsFigTreeNode with package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import FigTreeEditor, { type FigTreeEditorProps } from './FigTreeEditor'
+import { containsFigTreeNode } from './helpers'
 import { Select } from './Select'
 
-export { FigTreeEditor, Select, FigTreeEditorProps }
+export { FigTreeEditor, Select, containsFigTreeNode, FigTreeEditorProps }
 export * from 'fig-tree-evaluator'
