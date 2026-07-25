c0eed56aa259be99a4ecfa2c1474161efb49f66e
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2026-07-25 14:47:09 +1200

    Bump dependencies

--- demo/src/version.ts
+++ demo/src/version.ts
@@ -1,2 +1,2 @@
-export const figTreeEditorReactVersion = '1.0.0-beta.6'
-export const timestamp = '7/25/2026, 2:42:36 PM'
+export const figTreeEditorReactVersion = '1.0.0-beta.7'
+export const timestamp = '7/25/2026, 2:45:30 PM'
